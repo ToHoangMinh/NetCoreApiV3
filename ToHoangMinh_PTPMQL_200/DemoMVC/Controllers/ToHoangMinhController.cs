00a4018a90af4723952e6ed67aa75e0904688bea
ToHoangMinh <hoangminh170523@gmail.com>
2024-08-26 22:52:32 +0700

    done

---
+++ ToHoangMinh_PTPMQL_200/DemoMVC/Controllers/ToHoangMinhController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoMVC.Controllers;
+public class ToHoangMinhController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
